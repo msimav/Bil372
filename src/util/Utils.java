600a47e9094290f51bf74fd62ab802657e895d4e
Mustafa Simav <Mustafa1991@gmail.com>
2012-07-13 21:28:39 +0300

    toJSON and fromJSON functions implemented in Utils class

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -2,6 +2,8 @@ package util;
 
 import java.util.Date;
 
+import com.google.gson.Gson;
+
 public class Utils {
 
 	/**
@@ -11,7 +13,7 @@ public class Utils {
 	public static String getDate() {
 		return new Date().toString();
 	}
-	
+
 	/**
 	 * Sifreleri veritabaninda hashlanmis olarak saklamak lazim
 	 * http://stackoverflow.com/a/6565597/806261
@@ -20,16 +22,38 @@ public class Utils {
 	 */
 	public static String md5hash(String arg) {
 		try {
-	        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-	        md.reset();
-	        byte[] array = md.digest(arg.getBytes());
-	        StringBuffer sb = new StringBuffer();
-	        for (int i = 0; i < array.length; ++i) {
-	          sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
-	       }
-	        return sb.toString();
-	    } catch (java.security.NoSuchAlgorithmException e) {
-	    }
-	    return null;
+			java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+			md.reset();
+			byte[] array = md.digest(arg.getBytes());
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < array.length; ++i) {
+				sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+			}
+			return sb.toString();
+		} catch (java.security.NoSuchAlgorithmException e) {
+		}
+		return null;
+	}
+
+	/**
+	 * Verilen nesneyi json formatina cevirir.
+	 * @param object json formatina cevrilmek istenen nesne
+	 * @return json stringi
+	 */
+	public static <T> String toJSON(T object) {
+		Gson converter = new Gson();
+		return converter.toJson(object, object.getClass());
+	}
+
+	/**
+	 * Json olarak verilmis stringi nerekli nesneye cevirir.
+	 * @param json json kodlanmis string string
+	 * @param classOfT json stringinin karsilik geldigi nesne
+	 * @return karsilik gelen nesne
+	 */
+	public static <T> T fromJSON(String json, Class<T> classOfT) {
+		Gson converter = new Gson();
+		T data = converter.fromJson(json, classOfT);
+		return data;
 	}
 }
